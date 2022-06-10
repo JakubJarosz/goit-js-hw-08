c909aafe9f8b355c3effdcb6ddd0000c030d9eef
JakubJarosz <jakubtm165@gmail.com>
2022-06-10 20:10:45 +0200

    ad

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,22 @@
 import Player from '@vimeo/player';
-const _ = require('lodash.throttle');
 
+// const _ = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-
 const onPlay = function (data) {
-     
-    window.localStorage.setItem(
-      'videoplayer-current-time',
-      JSON.stringify(data.seconds)
-    )
+ throt_func(data.seconds)
 };
+const throt_func = throttle(function (e) {
+  window.localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
+}, 1000);
 
-player.on("timeupdate", onPlay);
+player.on('timeupdate', onPlay);
 
-
-
-const currentTime = JSON.parse(window.localStorage.getItem('videoplayer-current-time'));
+const currentTime = JSON.parse(
+  window.localStorage.getItem('videoplayer-current-time')
+);
 
 player.setCurrentTime(currentTime);
 
-
-const result = _.add(2, 3);
-console.log(result); // 5
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,44 @@
-const form = document.querySelector("form");
-const emailInput = document.querySelector("input");
-const messageInput = document.querySelector('textarea');    
+const form = document.querySelector('form');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+import throttle from 'lodash.throttle';
 
-
-form.addEventListener("input", (event) => {
-    const {
-        elements: { email, message
-        }
-    } = event.currentTarget;
-    const data = {};
-    data.email = email.value;
-    data.message = message.value;
-    window.localStorage.setItem(
-        'feedback-form-state',
-        JSON.stringify(data)
-    );
-})
-
+form.addEventListener('input', event => {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const data = {};
+  data.email = email.value;
+  data.message = message.value;
+  throt_func(data);
+});
+const throt_func = throttle(function (e) {
+  window.localStorage.setItem('feedback-form-state', JSON.stringify(e));
+}, 500);
 const dataJson = JSON.parse(window.localStorage.getItem('feedback-form-state'));
 emailInput.value = dataJson.email;
 messageInput.value = dataJson.message;
 
-
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const {
-        elements: { email, message }
-    } = event.currentTarget;
-    if (email.value === "" || message.value === "") {
-        return console.log('Please fill in all the fields!');
-    }
-    const dataJsonV2 = JSON.parse(
-      window.localStorage.getItem('feedback-form-state')
-    );
-    console.log(dataJsonV2);
-    dataJsonV2.email = "";
-    dataJsonV2.message = "";
-    window.localStorage.setItem('feedback-form-state', JSON.stringify(dataJsonV2));
-    emailInput.value = dataJson.email;
-    messageInput.value = dataJson.message;
-
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+  const dataJsonV2 = JSON.parse(
+    window.localStorage.getItem('feedback-form-state')
+  );
+  console.log(dataJsonV2);
+  dataJsonV2.email = '';
+  dataJsonV2.message = '';
+  window.localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(dataJsonV2)
+  );
+  emailInput.value = dataJson.email;
+  messageInput.value = dataJson.message;
 }
